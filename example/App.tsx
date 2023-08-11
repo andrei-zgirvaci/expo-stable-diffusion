3c4bf80090af4ec9aaca7341d8141d4343c5155c
7nohe <7nohe.urata@gmail.com>
2023-08-11 10:11:09 +0900

    feat: add progress tracking to example

--- example/App.tsx
+++ example/App.tsx
@@ -6,8 +6,10 @@ import * as ExpoStableDiffusion from "expo-stable-diffusion";
 
 const MODEL_PATH = FileSystem.documentDirectory + "Model/stable-diffusion-2-1";
 const SAVE_PATH = FileSystem.documentDirectory + "image.jpeg";
+const STEP_COUNT = 25;
 
 export default function App() {
+  const [currentStep, setCurrentStep] = React.useState(0);
   React.useEffect(() => {
     (async () => {
       Alert.alert(`Loading Model: ${MODEL_PATH}`);
@@ -16,19 +18,31 @@ export default function App() {
 
       Alert.alert("Model Loaded, Generating Images!");
 
+      setCurrentStep(0);
       await ExpoStableDiffusion.generateImage({
         prompt: "a cat coding at night",
-        stepCount: 25,
+        stepCount: STEP_COUNT,
         savePath: SAVE_PATH,
       });
 
       Alert.alert(`Image Generated: ${SAVE_PATH}`);
     })();
+
+    const timer = setInterval(() => {
+      setCurrentStep(ExpoStableDiffusion.getCurrentStep());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Testing Expo Stable Diffusion</Text>
+      <Text>
+        Progress: {currentStep}/{STEP_COUNT}
+      </Text>
     </View>
   );
 }
